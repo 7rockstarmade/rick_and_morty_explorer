4d68a74a7b6850b0d68cd9b77ab63cae06fc826e
7rockstarmade <243180667+7rockstarmade@users.noreply.github.com>
2026-03-08 13:09:29 +0300

    theme: create theme cubit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rick_and_morty_exporer/app/theme/app_theme.dart';
+import 'package:rick_and_morty_exporer/app/theme/theme_cubit.dart';
 
 class RickAndMortyExplorer extends StatelessWidget {
   const RickAndMortyExplorer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Rick & Morty Explorer',
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
-      home: Scaffold(body: Container()),
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Rick & Morty Explorer',
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: themeMode,
+            home: Scaffold(body: Container()),
+          );
+        },
+      ),
     );
   }
 }
